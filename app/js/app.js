ba578ffc343564ddf9118326f840594e310e9165
Bnhuser <Brenna@brennas-macbook.osustudent.okstate.edu>
2014-09-03 09:07:02 -0500

    Adding functionality

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,9 @@
 angular.module('myApp', [
     'ngRoute',
     'homeController',
-    'whySKController'
+    'whySKController',
+    'exampleController',
+    'moreController'
 ]).
 config(['$routeProvider', 
     function($routeProvider){
@@ -17,6 +19,14 @@ config(['$routeProvider',
     when('/home/whySK',{
         templateUrl:'app/partials/whySK.html',
         controller:'WhySKCtrl'
+    }).
+    when('/home/example',{
+        templateUrl:'app/partials/example.html',
+        controller:'exampleCtrl'
+    }).
+    when('/home/more',{
+        templateUrl:'app/partials/more.html',
+        controller:'moreCtrl'
     }).
     otherwise({
         redirectTo: '/home'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,8 @@
 
 var homeController = angular.module('homeController', []);
 var whySKController = angular.module('whySKController', []);
+var exampleController = angular.module('exampleController', []);
+var moreController = angular.module('moreController', []);
 
 
 //==============================
@@ -19,6 +21,24 @@ homeController.controller('MainCtrl', ['$scope', function($scope) {
 //-----Why SK controller-----
 //==============================
 homeController.controller('WhySKCtrl', ['$scope', function($scope) {
+       
+
+
+}]);
+
+//==============================
+//-----Why SK controller-----
+//==============================
+exampleController.controller('exampleCtrl', ['$scope', function($scope) {
+       
+
+
+}]);
+
+//==============================
+//-----Why SK controller-----
+//==============================
+moreController.controller('moreCtrl', ['$scope', function($scope) {
        
 
 
